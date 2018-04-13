4636ca9fe90dee8a524976fb57ffa7b4cbb5c3b0
James ODonnell <mr.odonnell.cte@gmail.com>
2018-04-13 05:53:54 -0400

    add new file demonstrating some uses of random

--- rand.py
+++ rand.py
@@ -1,9 +1,19 @@
+"""
+This is a very simple function that will return
+a random number from one to 6 by default
+and a demonstration of how to use it
+You must import random
+"""
+
 import random
 
-def die():
-        return random.randint(1,6)
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven']
+
+def rtn_rand(beg=1, end=6):
+        return random.randint(beg,end)
+
 count = 0
 while count < 100:
-    x = die()
-    print(x)
+    x = rtn_rand()
+    print(numbers[x])
     count += 1
